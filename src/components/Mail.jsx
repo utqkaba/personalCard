dd76554f88eb25abfd337b4e83fe7c0efba4e8eb
utqkaba <utqkaba@gmail.com>
2023-09-04 20:51:05 +0300

    removve mail span

--- src/components/Mail.jsx
+++ src/components/Mail.jsx
@@ -21,8 +21,8 @@ function Mail({ media, links }) {
     <div className='card-links'>
       <a href={links} target='_blank' rel="noreferrer" onClick={() => notifyCopyMail(mail)}>
         <div className='card-item flex justify-between bg-slate-100 bg-opacity-90 px-5 py-4 rounded-full mb-4 hover:bg-slate-50 hover:bg-opacity-100 hover:scale-105 duration-500 cursor-pointer drop-shadow-xl'>
-          <p className='name ml-1'>{media}:
-            <span className='italic'>  utqkaba@gmail.com</span>
+          <p className='name ml-1'>{media}
+            {/* <span className='italic'>  utqkaba@gmail.com</span> */}
           </p>
           <div className='icon mr-1'>
           </div>
