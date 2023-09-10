f3b39250b9b15525ce8382a04b7fe9939b2f4207
utqkaba <utqkaba@gmail.com>
2023-09-10 21:38:51 +0300

    card redesign

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,7 +4,7 @@ function Info({ media, links }) {
   return (
     <div className='card-links mx-8'>
       <a href={links} target='_blank' rel="noreferrer">
-        <div className='card-item flex justify-center bg-slate-100 bg-opacity-90 px-5 py-4 rounded-full mb-4 hover:bg-slate-50 hover:bg-opacity-100 hover:scale-105 duration-500 hover:blur-0 cursor-pointer drop-shadow-xl'>
+        <div className='card-item flex justify-center scale-90 bg-slate-100 bg-opacity-90 px-5 py-4 rounded-full mb-4 hover:bg-slate-50 hover:bg-opacity-100 hover:scale-105 duration-500 hover:blur-0 cursor-pointer drop-shadow-xl'>
           <p className='name ml-1 text-lg'>{media}</p>
           <div className='icon mr-1'>
           </div>
